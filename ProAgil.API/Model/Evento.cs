66c7839cd5b9efe689c3ec25184bfa8da59f07a4
Nakamura <lp_nakamura@hotmail.com>
2019-03-17 12:28:31 -0300

    first commit

---
+++ ProAgil.API/Model/Evento.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ProAgil.API.Model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string  Tema { get; set; }
+        public int QtdPessoa { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+
+
+    }
+}
